de8b226c20f6f6962d4d160c254a8eea525130e5
Francis Emmanuel Galguerra <galguerrafrancis@gmail.com>
2023-02-24 21:37:40 +0800

    + Scaffold Authentication using Laravel Breeze

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+require('jquery');
+
+import Alpine from 'alpinejs';
+import jQuery from 'jquery';
+
+window.Alpine = Alpine;
+window.jQuery = jQuery;
+window.$ = jQuery;
+
+Alpine.start();
